3a24cc6f2330045c97b2e6c66e4d0a7152bf6f24
Fellipe Juncal <fjuncal7@gmail.com>
2024-05-10 17:01:14 -0300

    Formating timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import TimerDisplay from "./TimerDisplay";
 import TimerControls from "./TimerControls";
 import LapList from "./LapList";
 import "./Timer.css";
 const Timer = () => {
+  const [milliseconds, setMilliseconds] = useState(0);
+  const [timerOn, setTimerOn] = useState(0);
+  const [laps, setLaps] = useState([]);
+
+  const formatTime = () => {
+    const minutes = ("0" + (Math.floor(milliseconds / 60000) % 60)).slice(-2);
+    const seconds = ("0" + (Math.floor(milliseconds / 1000) % 60)).slice(-2);
+    const centiSeconds = ("0" + (Math.floor(milliseconds / 10) % 100)).slice(
+      -2
+    );
+
+    return `${minutes}:${seconds}:${centiSeconds}`;
+  };
+  formatTime();
   return (
     <div className="timer-container">
-      <TimerDisplay />
+      <TimerDisplay time={formatTime()} />
       <TimerControls />
       <LapList />
     </div>
